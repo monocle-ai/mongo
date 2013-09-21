xiexiaoy/eloqdoc

SERVER-7572 Remove doauth argument from Client::Context

src/mongo/db/oplog.cpp
@@ -76,13 +76,13 @@ namespace mongo {
         {
             const char *logns = rsoplog;
             if ( rsOplogDetails == 0 ) {
-                Client::Context ctx( logns , dbpath, false);
+                Client::Context ctx(logns , dbpath);
                 localDB = ctx.db();
                 verify( localDB );
                 rsOplogDetails = nsdetails(logns);
                 massert(13389, "local.oplog.rs missing. did you drop it? if so restart server", rsOplogDetails);
             }
-            Client::Context ctx( logns , localDB, false );
+            Client::Context ctx(logns , localDB);
             {
                 int len = op.objsize();
                 Record *r = theDataFileMgr.fast_oplog_insert(rsOplogDetails, logns, len);
@@ -207,13 +207,13 @@ namespace mongo {
         {
             const char *logns = rsoplog;
             if ( rsOplogDetails == 0 ) {
-                Client::Context ctx( logns , dbpath, false);
+                Client::Context ctx(logns , dbpath);
                 localDB = ctx.db();
                 verify( localDB );
                 rsOplogDetails = nsdetails(logns);
                 massert(13347, "local.oplog.rs missing. did you drop it? if so restart server", rsOplogDetails);
             }
-            Client::Context ctx( logns , localDB, false );
+            Client::Context ctx(logns , localDB);
             r = theDataFileMgr.fast_oplog_insert(rsOplogDetails, logns, len);
             /* todo: now() has code to handle clock skew.  but if the skew server to server is large it will get unhappy.
                      this code (or code in now() maybe) should be improved.
@@ -250,7 +250,7 @@ namespace mongo {
         mutex::scoped_lock lk2(OpTime::m);
 
         const OpTime ts = OpTime::now(lk2);
-        Client::Context context("",0,false);
+        Client::Context context("", 0);
 
         /* we jump through a bunch of hoops here to avoid copying the obj buffer twice --
            instead we do a single copy to the destination position in the memory mapped file.
@@ -276,17 +276,17 @@ namespace mongo {
         if( logNS == 0 ) {
             logNS = "local.oplog.$main";
             if ( localOplogMainDetails == 0 ) {
-                Client::Context ctx( logNS , dbpath, false);
+                Client::Context ctx(logNS , dbpath);
                 localDB = ctx.db();
                 verify( localDB );
                 localOplogMainDetails = nsdetails(logNS);
                 verify( localOplogMainDetails );
             }
-            Client::Context ctx( logNS , localDB, false );
+            Client::Context ctx(logNS , localDB);
             r = theDataFileMgr.fast_oplog_insert(localOplogMainDetails, logNS, len);
         }
         else {
-            Client::Context ctx( logNS, dbpath, false );
+            Client::Context ctx(logNS, dbpath);
             verify( nsdetails( logNS ) );
             // first we allocate the space, then we fill it below.
             r = theDataFileMgr.fast_oplog_insert( nsdetails( logNS ), logNS, len);
@@ -945,7 +945,7 @@ namespace mongo {
                 BSONElement e = i.next();
                 const BSONObj& temp = e.Obj();
                 
-                Client::Context ctx( temp["ns"].String() ); // this handles security
+                Client::Context ctx(temp["ns"].String());
                 bool failed = applyOperation_inlock(temp, false, alwaysUpsert);
                 ab.append(!failed);
                 if ( failed )

monocle-ai/mongo

Conflicts:
	src/mongo/db/client.cpp
	src/mongo/db/client.h
	src/mongo/db/cloner.cpp
	src/mongo/db/commands/mr.cpp
	src/mongo/db/dbcommands.cpp
	src/mongo/db/dbhelpers.cpp
	src/mongo/db/dbhelpers.h
	src/mongo/db/index_rebuilder.cpp
	src/mongo/db/introspect.cpp
	src/mongo/db/oplog.cpp
	src/mongo/db/repl.cpp
	src/mongo/db/repl/rs_sync.cpp
	src/mongo/db/restapi.cpp
Former-commit-id: 99f5e6d5c03a676738aefccc4936087a682c3b8a

src/mongo/db/oplog.cpp
@@ -49,7 +49,7 @@ namespace mongo {
         rsOplogRefsDetails = NULL;
         replInfoDetails = NULL;
         
-        Client::Context ctx( rsoplog, dbpath, false);
+        Client::Context ctx(rsoplog, dbpath);
         // TODO: code review this for possible error cases
         // although, I don't think we care about error cases,
         // just that after we exit, oplog files don't exist
@@ -63,17 +63,17 @@ namespace mongo {
     void openOplogFiles() {
         const char *logns = rsoplog;
         if (rsOplogDetails == NULL) {
-            Client::Context ctx(logns , dbpath, false);
+            Client::Context ctx(logns , dbpath);
             rsOplogDetails = nsdetails(logns);
             massert(13347, "local.oplog.rs missing. did you drop it? if so restart server", rsOplogDetails);
         }
         if (rsOplogRefsDetails == NULL) {
-            Client::Context ctx(rsOplogRefs , dbpath, false);
+            Client::Context ctx(rsOplogRefs , dbpath);
             rsOplogRefsDetails = nsdetails(rsOplogRefs);
             massert(16814, "local.oplog.refs missing. did you drop it? if so restart server", rsOplogRefsDetails);
         }
         if (replInfoDetails == NULL) {
-            Client::Context ctx(rsReplInfo , dbpath, false);
+            Client::Context ctx(rsReplInfo , dbpath);
             replInfoDetails = nsdetails(rsReplInfo);
             massert(16747, "local.replInfo missing. did you drop it? if so restart server", replInfoDetails);
         }