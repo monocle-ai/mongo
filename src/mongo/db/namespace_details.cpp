monocle-ai/mongo

commit 4ac25c5c8fd714d3cda3db1d136b4836ab7b7f13

fixed behavior of dropDatabase when profiling is on #546
fixes #546


Former-commit-id: 5991d79e3a732668668cb1b8c0e42a923ee35bc3

src/mongo/db/namespace_details.cpp
@@ -1658,12 +1658,11 @@ namespace mongo {
         StringData database = nsToDatabaseSubstring(name);
         verify(database == cc().database()->name());
         StringData coll = name.substr(name.find('.') + 1);
-        if (coll.startsWith("system.")) {
+        if (coll.startsWith("system.") && !can_drop_system) {
             if (coll == "system.profile") {
                 uassert(10087, "turn off profiling before dropping system.profile collection", cc().database()->profile() == 0);
-            } else if (!can_drop_system) {
-                uasserted(12502, "can't drop system ns");
             }
+            uasserted(12502, "can't drop system ns");
         }
 
         // Invalidate cursors, then drop all of the indexes.

commit 06d2493af094844c345eeddd9056d7458562b5ae

re-allow dropping of system.profile collection #546
Originally broken in 4ac25c5c8fd714d3cda3db1d136b4836ab7b7f13 [formerly 5991d79e3a732668668cb1b8c0e42a923ee35bc3].

fixes #546


Former-commit-id: 01fc2255c222fb9ca9b5f44d97f27573eb32a939

src/mongo/db/namespace_details.cpp
@@ -1661,8 +1661,9 @@ namespace mongo {
         if (coll.startsWith("system.") && !can_drop_system) {
             if (coll == "system.profile") {
                 uassert(10087, "turn off profiling before dropping system.profile collection", cc().database()->profile() == 0);
+            } else {
+                uasserted(12502, "can't drop system ns");
             }
-            uasserted(12502, "can't drop system ns");
         }
 
         // Invalidate cursors, then drop all of the indexes.