xiexiaoy/eloqdoc

SERVER-7984 Fix Windows compile
Work around apparent MSVC compiler bug by explicitly casting from
'const char []' to 'const char*'.

src/mongo/s/config_upgrade.cpp
@@ -260,8 +260,9 @@ namespace mongo {
         // versionString is the global version of this process
         if (isInMongoVersionRanges(versionString, excludedRanges)) {
 
-            *whyNot = stream() << "not compatible with current config version, " << "version "
-                               << versionString << "has been excluded.";
+            *whyNot = stream() << "not compatible with current config version, version "
+                               << reinterpret_cast<const char*>(versionString)
+                               << "has been excluded.";
 
             return VersionStatus_Incompatible;
         }

monocle-ai/mongo

Former-commit-id: 401324fd2a519b619c50494b031f81bb623d6d67

src/mongo/s/config_upgrade.cpp
@@ -261,7 +261,8 @@ namespace mongo {
         if (isInMongoVersionRanges(mongodbVersionString, excludedRanges)) {
 
             *whyNot = stream() << "not compatible with current config version, version "
-                               << mongodbVersionString << "has been excluded.";
+                               << reinterpret_cast<const char*>(mongodbVersionString)
+                               << "has been excluded.";
 
             return VersionStatus_Incompatible;
         }