xiexiaoy/eloqdoc

nsdetails takes a StringData, don't call c_str on std::string

src/mongo/db/dbhelpers.cpp
@@ -290,7 +290,7 @@ namespace mongo {
                 scoped_ptr<Cursor> c;
                 
                 {
-                    NamespaceDetails* nsd = nsdetails( ns.c_str() );
+                    NamespaceDetails* nsd = nsdetails( ns );
                     if ( ! nsd )
                         break;
                     

monocle-ai/mongo

Conflicts:
	src/mongo/db/btree.cpp
	src/mongo/db/cloner.cpp
	src/mongo/db/commands/index_stats.cpp
	src/mongo/db/commands/storage_details.cpp
	src/mongo/db/commands/touch.cpp
	src/mongo/db/compact.cpp
	src/mongo/db/dbcommands.cpp
	src/mongo/db/dbhelpers.cpp
	src/mongo/db/geo/2d.cpp
	src/mongo/db/geo/geonear.cpp
	src/mongo/db/geo/haystack.cpp
	src/mongo/db/geo/s2cursor.cpp
	src/mongo/db/geo/s2nearcursor.cpp
	src/mongo/db/hashindex.cpp
	src/mongo/db/index.cpp
	src/mongo/db/index_update.cpp
	src/mongo/db/namespace_details.cpp
	src/mongo/db/pdfile.cpp
	src/mongo/db/query_optimizer_internal.cpp
	src/mongo/db/ttl.cpp
	src/mongo/dbtests/btreetests.inl
	src/mongo/s/d_migrate.cpp
	src/mongo/s/d_split.cpp
	src/mongo/tools/dump.cpp
	src/mongo/util/touch_pages.cpp
Former-commit-id: 44ede1942bb5faa2a705255e199293f07dba65c4

src/mongo/db/dbhelpers.cpp
@@ -107,7 +107,7 @@ namespace mongo {
         Client::ReadContext ctx(ns);
         Client::Transaction txn(DB_SERIALIZABLE);
 
-        NamespaceDetails *d = nsdetails( ns.c_str() );
+        NamespaceDetails *d = nsdetails(ns);
         IndexDetails &i = d->idx(d->findIndexByKeyPattern(keyPattern));
         // Extend min to get (min, MinKey, MinKey, ....)
         BSONObj newMin = Helpers::modifiedRangeBound( min , keyPattern , -1 );