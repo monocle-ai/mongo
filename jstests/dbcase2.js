xiexiaoy/eloqdoc

SERVER-7698 avoid collisions between dbcase.js and dbcase2.js

jstests/dbcase2.js
@@ -1,9 +1,9 @@
 // SERVER-2111 Check that an in memory db name will block creation of a db with a similar but differently cased name.
 
-a = db.getSisterDB( "dbcasetest_dbnamea" )
-b = db.getSisterDB( "dbcasetest_dbnameA" )
+a = db.getSisterDB( "dbcase2test_dbnamea" )
+b = db.getSisterDB( "dbcase2test_dbnameA" )
 
 a.c.count();
 assert.throws( function() { b.c.count() } );
 
-assert.eq( -1, db.getMongo().getDBNames().indexOf( "dbcasetest_dbnameA" ) );
+assert.eq( -1, db.getMongo().getDBNames().indexOf( "dbcase2test_dbnameA" ) );

monocle-ai/mongo

Conflicts:
	jstests/dbcase2.js
Former-commit-id: 2057475b19cd113674e9b80a33b7bf22ab4d159a

jstests/dbcase2.js
@@ -1,10 +1,10 @@
 // SERVER-2111 Check that an in memory db name will block creation of a db with a similar but differently cased name.
 
-a = db.getSisterDB( "dbcasetest_dbnamea" )
-b = db.getSisterDB( "dbcasetest_dbnameA" )
+a = db.getSisterDB( "dbcase2test_dbnamea" )
+b = db.getSisterDB( "dbcase2test_dbnameA" )
 
 a.c.count();
 b.c.count();
 //assert.throws( function() { b.c.count() } );
 
-assert.neq( -1, db.getMongo().getDBNames().indexOf( "dbcasetest_dbnameA" ) );
+assert.neq( -1, db.getMongo().getDBNames().indexOf( "dbcase2test_dbnameA" ) );