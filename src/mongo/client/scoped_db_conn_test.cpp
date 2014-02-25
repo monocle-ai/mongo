xiexiaoy/eloqdoc

SERVER-9818 allocate socket file descriptors before opening datafiles

src/mongo/client/scoped_db_conn_test.cpp
@@ -139,6 +139,7 @@ namespace mongo_test {
             }
 
             _server = mongo::createServer(options, messsageHandler);
+            _server->setupSockets();
             _serverThread = boost::thread(runServer, _server);
         }
 

monocle-ai/mongo

Conflicts:
	src/mongo/db/curop_test.cpp
	src/mongo/util/net/listen.h
Former-commit-id: 2192af46bc9417bd5577c6e035496b3ce44af690

src/mongo/client/scoped_db_conn_test.cpp
@@ -178,6 +178,7 @@ namespace mongo_test {
          * Helper method for running the server on a separate thread.
          */
         static void runServer(mongo::MessageServer* server) {
+            server->setupSockets();
             server->run();
         }
 
@@ -275,6 +276,7 @@ namespace mongo_test {
 
     private:
         static void runServer(mongo::MessageServer* server) {
+            server->setupSockets();
             server->run();
         }
 