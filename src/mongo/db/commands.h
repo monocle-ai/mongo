xiexiaoy/eloqdoc

SERVER-7122 Make copydb not work with auth on mongos

src/mongo/db/commands.h
@@ -163,6 +163,31 @@ namespace mongo {
         static int testCommandsEnabled;
     };
 
+    // This will be registered instead of the real implementations of any commands that don't work
+    // when auth is enabled.
+    class NotWithAuthCmd : public Command {
+    public:
+        NotWithAuthCmd(const char* cmdName) : Command(cmdName) { }
+        virtual bool slaveOk() const { return true; }
+        virtual LockType locktype() const { return NONE; }
+        virtual bool requiresAuth() { return false; }
+        virtual void addRequiredPrivileges(const std::string& dbname,
+                                           const BSONObj& cmdObj,
+                                           std::vector<Privilege>* out) {}
+        virtual void help( stringstream &help ) const {
+            help << name << " is not supported when running with authentication enabled";
+        }
+        virtual bool run(const string&,
+                         BSONObj& cmdObj,
+                         int,
+                         string& errmsg,
+                         BSONObjBuilder& result,
+                         bool fromRepl) {
+            errmsg = name + " is not supported when running with authentication enabled";
+            return false;
+        }
+    };
+
     class CmdShutdown : public Command {
     public:
         virtual bool requiresAuth() { return true; }

monocle-ai/mongo

Conflicts:
	src/mongo/db/cloner.cpp
	src/mongo/db/commands.h
	src/mongo/s/commands_public.cpp
Former-commit-id: 8ffe14cf3db9d29760998e55ef46e6448b07688f

src/mongo/db/commands.h
@@ -305,6 +305,29 @@ namespace mongo {
         }
     };
 
+    // This will be registered instead of the real implementations of any commands that don't work
+    // when auth is enabled.
+    class NotWithAuthCmd : public InformationCommand {
+    public:
+        NotWithAuthCmd(const char* cmdName) : InformationCommand(cmdName) { }
+        virtual bool requiresAuth() { return false; }
+        virtual void addRequiredPrivileges(const std::string& dbname,
+                                           const BSONObj& cmdObj,
+                                           std::vector<Privilege>* out) {}
+        virtual void help( stringstream &help ) const {
+            help << name << " is not supported when running with authentication enabled";
+        }
+        virtual bool run(const string&,
+                         BSONObj& cmdObj,
+                         int,
+                         string& errmsg,
+                         BSONObjBuilder& result,
+                         bool fromRepl) {
+            errmsg = name + " is not supported when running with authentication enabled";
+            return false;
+        }
+    };
+
     class CmdShutdown : public Command {
     public:
         virtual bool requiresAuth() { return true; }