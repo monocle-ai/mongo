xiexiaoy/eloqdoc

SERVER-10084 New logging implementation.
This change-set:

* Introduces a new top-level directory, mongo/logger, containing most of the implementation of logging functionality formerly found in log.cpp/log.h.

* Cleans up existing, unusual uses of the logging system that were not trivially compatible with the new implementation.

* Replaces Logstream/Nulstream with a LogstreamBuilder object, whose destructor writes log messages.  This new LogstreamBuilder is reentrant, unlike the old logging code, which was thread-safe but not reentrant.  Additionally, std::endl is no longer required to terminate a log line.  When a LogstreamBuilder goes out of scope, the log message gets committed.

* Separates the log system into several components: a global LogManager, several LogDomains, various kinds of Appenders (e.g., SyslogAppender) and Encoders (for formatting messages).

* Allows unit tests to capture and examine log output.

This patch does _not_ introduce support for hierarchical log domains, or for enabling and disabling specific log domains when the server is running in a multi-threaded mode.  This is future work.

src/mongo/s/s_only.cpp
@@ -15,20 +15,21 @@
  *    limitations under the License.
  */
 
-#include "pch.h"
+#include "mongo/pch.h"
 
 #include "mongo/client/connpool.h"
 #include "mongo/db/auth/authorization_manager.h"
 #include "mongo/db/auth/authorization_manager_global.h"
 #include "mongo/db/auth/authorization_session.h"
 #include "mongo/db/auth/authz_session_external_state_s.h"
-#include "mongo/s/shard.h"
+#include "mongo/db/commands.h"
+#include "mongo/db/dbhelpers.h"
+#include "mongo/db/matcher.h"
+#include "mongo/s/client_info.h"
 #include "mongo/s/grid.h"
-#include "request.h"
-#include "client_info.h"
-#include "../db/dbhelpers.h"
-#include "../db/matcher.h"
-#include "../db/commands.h"
+#include "mongo/s/request.h"
+#include "mongo/s/shard.h"
+#include "mongo/util/concurrency/thread_name.h"
 
 /*
   most a pile of hacks to make linking nicer

monocle-ai/mongo

Conflicts:
	src/SConscript.client
	src/mongo/SConscript
	src/mongo/base/error_codes.err
	src/mongo/client/distlock.cpp
	src/mongo/db/btree.cpp
	src/mongo/db/btreebuilder.cpp
	src/mongo/db/client.cpp
	src/mongo/db/commands/index_stats.cpp
	src/mongo/db/commands/mr.cpp
	src/mongo/db/commands/parameters.cpp
	src/mongo/db/commands/pipeline_command.cpp
	src/mongo/db/commands/storage_details.cpp
	src/mongo/db/commands/write_commands/batch_executor.cpp
	src/mongo/db/database.cpp
	src/mongo/db/db.cpp
	src/mongo/db/dbcommands.cpp
	src/mongo/db/dbcommands_admin.cpp
	src/mongo/db/index.cpp
	src/mongo/db/index/btree_based_builder.cpp
	src/mongo/db/index_update.cpp
	src/mongo/db/initialize_server_global_state.cpp
	src/mongo/db/instance.cpp
	src/mongo/db/ops/query.cpp
	src/mongo/db/pagefault.cpp
	src/mongo/db/pdfile.cpp
	src/mongo/db/pipeline/pipeline_d.cpp
	src/mongo/db/record.cpp
	src/mongo/db/repl/master_slave.cpp
	src/mongo/db/repl/oplog.cpp
	src/mongo/db/storage/namespace_index.cpp
	src/mongo/dbtests/basictests.cpp
	src/mongo/dbtests/extsorttests.cpp
	src/mongo/dbtests/mmaptests.cpp
	src/mongo/dbtests/perf/perftest.cpp
	src/mongo/dbtests/perftests.cpp
	src/mongo/dbtests/queryoptimizertests2.cpp
	src/mongo/dbtests/replsettests.cpp
	src/mongo/dbtests/repltests.cpp
	src/mongo/s/commands_admin.cpp
	src/mongo/s/s_only.cpp
	src/mongo/tools/tool.cpp
	src/mongo/util/file_allocator.cpp
	src/mongo/util/log.cpp
	src/mongo/util/log.h
	src/mongo/util/progress_meter.cpp
	src/mongo/util/version.cpp
Former-commit-id: e846831c59e5b91777f504edcbd241f9a67869c0

src/mongo/s/s_only.cpp
@@ -23,13 +23,14 @@
 #include "mongo/db/auth/authorization_manager_global.h"
 #include "mongo/db/auth/authorization_session.h"
 #include "mongo/db/auth/authz_session_external_state_s.h"
-#include "mongo/s/shard.h"
+#include "mongo/db/commands.h"
+#include "mongo/db/matcher.h"
+#include "mongo/db/namespacestring.h"
+#include "mongo/s/client_info.h"
 #include "mongo/s/grid.h"
 #include "mongo/s/request.h"
-#include "mongo/s/client_info.h"
-#include "mongo/db/matcher.h"
-#include "mongo/db/commands.h"
-#include "mongo/db/namespacestring.h"
+#include "mongo/s/shard.h"
+#include "mongo/util/concurrency/thread_name.h"
 
 /*
   most a pile of hacks to make linking nicer