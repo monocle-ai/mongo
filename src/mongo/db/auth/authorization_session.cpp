xiexiaoy/eloqdoc

SERVER-10596 Globalize formerly per-thread Pool of JS Scopes
This ensures that the limit of 10 pooled scopes is actually enforced.
With a per-thread Pool, long-lived connections could cause very high
memory usage (both real and virtual) even if they haven't used JS in a
long time.

src/mongo/db/auth/authorization_session.cpp
@@ -72,7 +72,6 @@ namespace {
             getAuthorizationManager().releaseUser(replacedUser);
         }
 
-        _externalState->onAddAuthorizedUser();
         return Status::OK();
     }
 
@@ -85,12 +84,22 @@ namespace {
         if (removedUser) {
             getAuthorizationManager().releaseUser(removedUser);
         }
-
-        _externalState->onLogoutDatabase(dbname);
     }
 
     UserSet::NameIterator AuthorizationSession::getAuthenticatedUserNames() {
         return _authenticatedUsers.getNames();
+    }
+
+    std::string AuthorizationSession::getAuthenticatedUserNamesToken() {
+        std::string ret;
+        for (UserSet::NameIterator nameIter = getAuthenticatedUserNames();
+                nameIter.more();
+                nameIter.next()) {
+            ret += '\0'; // Using a NUL byte which isn't valid in usernames to separate them.
+            ret += nameIter->getFullName();
+        }
+
+        return ret;
     }
 
     void AuthorizationSession::grantInternalAuthorization() {

src/mongo/db/auth/authorization_session.h
@@ -68,6 +68,10 @@ namespace mongo {
         // Gets an iterator over the names of all authenticated users stored in this manager.
         UserSet::NameIterator getAuthenticatedUserNames();
 
+        // Returns a string representing all logged-in users on the current session.
+        // WARNING: this string will contain NUL bytes so don't call c_str()!
+        std::string getAuthenticatedUserNamesToken();
+
         // Removes any authenticated principals whose authorization credentials came from the given
         // database, and revokes any privileges that were granted via that principal.
         void logoutDatabase(const std::string& dbname);

monocle-ai/mongo

Conflicts:
	jstests/auth/js_scope_leak.js
	src/mongo/db/auth/authorization_session.cpp
	src/mongo/db/auth/authz_session_external_state_d.h
	src/mongo/db/auth/authz_session_external_state_mock.h
	src/mongo/db/auth/authz_session_external_state_s.h
	src/mongo/db/commands/group.cpp
	src/mongo/db/commands/mr.cpp
	src/mongo/db/dbeval.cpp
	src/mongo/db/matcher/expression_where.cpp
Former-commit-id: b669f9d74d2c5bf692f27eb8e759c48a6f49e37a

src/mongo/db/auth/authorization_session.cpp
@@ -126,6 +126,18 @@ namespace {
                                     principal->getName()).isOK());
     }
 
+    std::string AuthorizationSession::getAuthenticatedPrincipalNamesToken() {
+        std::string ret;
+        for (PrincipalSet::NameIterator nameIter = getAuthenticatedPrincipalNames();
+                nameIter.more();
+                nameIter.next()) {
+            ret += '\0'; // Using a NUL byte which isn't valid in usernames to separate them.
+            ret += nameIter->getFullName();
+        }
+
+        return ret;
+    }
+
     bool AuthorizationSession::hasInternalAuthorization() {
         ActionSet allActions;
         allActions.addAllActions();

src/mongo/db/auth/authorization_session.h
@@ -66,6 +66,10 @@ namespace mongo {
         // Gets an iterator over the names of all authenticated principals stored in this manager.
         PrincipalSet::NameIterator getAuthenticatedPrincipalNames();
 
+        // Returns a string representing all logged-in users on the current session.
+        // WARNING: this string will contain NUL bytes so don't call c_str()!
+        std::string getAuthenticatedPrincipalNamesToken();
+
         // Removes any authenticated principals whose authorization credentials came from the given
         // database, and revokes any privileges that were granted via that principal.
         void logoutDatabase(const std::string& dbname);