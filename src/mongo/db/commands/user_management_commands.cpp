xiexiaoy/eloqdoc

SERVER-6246 Add usersInfo command

src/mongo/db/commands/user_management_commands.cpp
@@ -26,6 +26,7 @@
 *    it in the license file.
 */
 
+#include <boost/function.hpp>
 #include <string>
 #include <vector>
 
@@ -451,7 +452,7 @@ namespace mongo {
             return true;
         }
 
-    } CmdGrantRolesToUser;
+    } cmdGrantRolesToUser;
 
     class CmdRevokeRolesFromUser: public Command {
     public:
@@ -541,7 +542,7 @@ namespace mongo {
             return true;
         }
 
-    } CmdRevokeRolesFromUser;
+    } cmdRevokeRolesFromUser;
 
     class CmdGrantDelegateRolesToUser: public Command {
     public:
@@ -623,7 +624,7 @@ namespace mongo {
             return true;
         }
 
-    } CmdGrantDelegateRolesToUser;
+    } cmdGrantDelegateRolesToUser;
 
     class CmdRevokeDelegateRolesFromUser: public Command {
     public:
@@ -713,5 +714,85 @@ namespace mongo {
             return true;
         }
 
-    } CmdRevokeDelegateRolesFromUser;
+    } cmdRevokeDelegateRolesFromUser;
+
+    class CmdUsersInfo: public Command {
+    public:
+
+        virtual bool logTheOp() {
+            return false;
+        }
+
+        virtual bool slaveOk() const {
+            return false;
+        }
+
+        virtual LockType locktype() const {
+            return NONE;
+        }
+
+        CmdUsersInfo() : Command("usersInfo") {}
+
+        virtual void help(stringstream& ss) const {
+            ss << "Returns information about users." << endl;
+        }
+
+        virtual void addRequiredPrivileges(const std::string& dbname,
+                                           const BSONObj& cmdObj,
+                                           std::vector<Privilege>* out) {
+            // TODO: update this with the new rules around user creation in 2.6.
+            ActionSet actions;
+            actions.addAction(ActionType::userAdmin);
+            out->push_back(Privilege(dbname, actions));
+        }
+
+        bool run(const string& dbname,
+                 BSONObj& cmdObj,
+                 int options,
+                 string& errmsg,
+                 BSONObjBuilder& result,
+                 bool fromRepl) {
+            if (cmdObj["usersInfo"].type() != String && cmdObj["usersInfo"].type() != RegEx) {
+                addStatus(Status(ErrorCodes::BadValue,
+                                 "Argument to userInfo command must be either a string or a regex"),
+                          result);
+                return false;
+            }
+
+            bool anyDB = false;
+            if (cmdObj.hasField("anyDB")) {
+                if (dbname == "admin") {
+                    Status status = bsonExtractBooleanField(cmdObj, "anyDB", &anyDB);
+                    if (!status.isOK()) {
+                        addStatus(status, result);
+                        return false;
+                    }
+                } else {
+                    addStatus(Status(ErrorCodes::BadValue,
+                                     "\"anyDB\" argument to usersInfo command is only valid when "
+                                     "run on the \"admin\" database"),
+                              result);
+                    return false;
+                }
+            }
+
+            BSONObjBuilder queryBuilder;
+            queryBuilder.appendAs(cmdObj["usersInfo"], "name");
+            if (!anyDB) {
+                queryBuilder.append("source", dbname);
+            }
+
+            BSONArrayBuilder usersArrayBuilder;
+            const boost::function<void(const BSONObj&)> function =
+                    boost::bind(&BSONArrayBuilder::append<BSONObj>, &usersArrayBuilder, _1);
+            AuthorizationManager* authzManager = getGlobalAuthorizationManager();
+            authzManager->queryAuthzDocument(NamespaceString("admin.system.users"),
+                                             queryBuilder.done(),
+                                             function);
+
+            result.append("users", usersArrayBuilder.arr());
+            return true;
+        }
+
+    } cmdUsersInfo;
 }

monocle-ai/mongo

Former-commit-id: 39546c7b56b3769c5c1bae3070dfa57a6a914a41

src/mongo/db/commands/user_management_commands.cpp
@@ -14,6 +14,7 @@
 *    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
+#include <boost/function.hpp>
 #include <string>
 #include <vector>
 
@@ -401,7 +402,7 @@ namespace mongo {
             return true;
         }
 
-    } CmdGrantRolesToUser;
+    } cmdGrantRolesToUser;
 
     class CmdRevokeRolesFromUser: public InformationCommand {
     public:
@@ -483,7 +484,7 @@ namespace mongo {
             return true;
         }
 
-    } CmdRevokeRolesFromUser;
+    } cmdRevokeRolesFromUser;
 
     class CmdGrantDelegateRolesToUser: public InformationCommand {
     public:
@@ -557,7 +558,7 @@ namespace mongo {
             return true;
         }
 
-    } CmdGrantDelegateRolesToUser;
+    } cmdGrantDelegateRolesToUser;
 
     class CmdRevokeDelegateRolesFromUser: public InformationCommand {
     public:
@@ -639,5 +640,77 @@ namespace mongo {
             return true;
         }
 
-    } CmdRevokeDelegateRolesFromUser;
+    } cmdRevokeDelegateRolesFromUser;
+
+    class CmdUsersInfo: public InformationCommand {
+    public:
+
+        virtual bool slaveOk() const {
+            return false;
+        }
+
+        CmdUsersInfo() : InformationCommand("usersInfo") {}
+
+        virtual void help(stringstream& ss) const {
+            ss << "Returns information about users." << endl;
+        }
+
+        virtual void addRequiredPrivileges(const std::string& dbname,
+                                           const BSONObj& cmdObj,
+                                           std::vector<Privilege>* out) {
+            // TODO: update this with the new rules around user creation in 2.6.
+            ActionSet actions;
+            actions.addAction(ActionType::userAdmin);
+            out->push_back(Privilege(dbname, actions));
+        }
+
+        bool run(const string& dbname,
+                 BSONObj& cmdObj,
+                 int options,
+                 string& errmsg,
+                 BSONObjBuilder& result,
+                 bool fromRepl) {
+            if (cmdObj["usersInfo"].type() != String && cmdObj["usersInfo"].type() != RegEx) {
+                addStatus(Status(ErrorCodes::BadValue,
+                                 "Argument to userInfo command must be either a string or a regex"),
+                          result);
+                return false;
+            }
+
+            bool anyDB = false;
+            if (cmdObj.hasField("anyDB")) {
+                if (dbname == "admin") {
+                    Status status = bsonExtractBooleanField(cmdObj, "anyDB", &anyDB);
+                    if (!status.isOK()) {
+                        addStatus(status, result);
+                        return false;
+                    }
+                } else {
+                    addStatus(Status(ErrorCodes::BadValue,
+                                     "\"anyDB\" argument to usersInfo command is only valid when "
+                                     "run on the \"admin\" database"),
+                              result);
+                    return false;
+                }
+            }
+
+            BSONObjBuilder queryBuilder;
+            queryBuilder.appendAs(cmdObj["usersInfo"], "name");
+            if (!anyDB) {
+                queryBuilder.append("source", dbname);
+            }
+
+            BSONArrayBuilder usersArrayBuilder;
+            const boost::function<void(const BSONObj&)> function =
+                    boost::bind(&BSONArrayBuilder::append<BSONObj>, &usersArrayBuilder, _1);
+            AuthorizationManager* authzManager = getGlobalAuthorizationManager();
+            authzManager->queryAuthzDocument(NamespaceString("admin.system.users"),
+                                             queryBuilder.done(),
+                                             function);
+
+            result.append("users", usersArrayBuilder.arr());
+            return true;
+        }
+
+    } cmdUsersInfo;
 }