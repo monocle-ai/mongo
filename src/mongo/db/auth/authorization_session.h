xiexiaoy/eloqdoc

SERVER-1105 Use ResourcePattern type when identifying the resource component of required privileges.
This patch has two principal components.  First, it changes the interface to Privilege and
AuthorizationSession to use ResourcePattern in place of std::string for identifying resources.
Second, it examines all call sites of the authorization session interface in commands and
other code to ensure that the correct resource requirements are conveyed to the authorization_session.

src/mongo/db/auth/authorization_session.h
@@ -40,15 +40,19 @@
 #include "mongo/db/auth/privilege.h"
 #include "mongo/db/auth/user_name.h"
 #include "mongo/db/auth/user_set.h"
+#include "mongo/db/namespace_string.h"
 
 namespace mongo {
 
     /**
      * Contains all the authorization logic for a single client connection.  It contains a set of
-     * the principals which have been authenticated, as well as a set of privileges that have been
-     * granted by those principals to perform various actions.
-     * An AuthorizationSession object is present within every mongo::Client object, therefore there
-     * is one per thread that corresponds to an incoming client connection.
+     * the users which have been authenticated, as well as a set of privileges that have been
+     * granted to those users to perform various actions.
+     *
+     * An AuthorizationSession object is present within every mongo::ClientBasic object.
+     *
+     * Predicate methods for checking authorization may in the worst case acquire read locks
+     * on the admin database.
      */
     class AuthorizationSession {
         MONGO_DISALLOW_COPYING(AuthorizationSession);
@@ -92,27 +96,17 @@ namespace mongo {
         // Used to grant internal threads full access.
         void grantInternalAuthorization();
 
-        // Checks if this connection has the privileges required to perform the given action
-        // on the given resource.  Contains all the authorization logic including handling things
-        // like the localhost exception.  Returns true if the action may proceed on the resource.
-        // Note: this may acquire a database read lock (for automatic privilege acquisition).
-        bool checkAuthorization(const std::string& resource, ActionType action);
-
-        // Same as above but takes an ActionSet instead of a single ActionType.  Returns true if
-        // all of the actions may proceed on the resource.
-        bool checkAuthorization(const std::string& resource, ActionSet actions);
-
         // Checks if this connection has the privileges necessary to perform the given query on the
         // given namespace.
-        Status checkAuthForQuery(const std::string& ns, const BSONObj& query);
+        Status checkAuthForQuery(const NamespaceString& ns, const BSONObj& query);
 
         // Checks if this connection has the privileges necessary to perform a getMore on the given
         // cursor in the given namespace.
-        Status checkAuthForGetMore(const std::string& ns, long long cursorID);
+        Status checkAuthForGetMore(const NamespaceString& ns, long long cursorID);
 
         // Checks if this connection has the privileges necessary to perform the given update on the
         // given namespace.
-        Status checkAuthForUpdate(const std::string& ns,
+        Status checkAuthForUpdate(const NamespaceString& ns,
                                   const BSONObj& query,
                                   const BSONObj& update,
                                   bool upsert);
@@ -120,29 +114,43 @@ namespace mongo {
         // Checks if this connection has the privileges necessary to insert the given document
         // to the given namespace.  Correctly interprets inserts to system.indexes and performs
         // the proper auth checks for index building.
-        Status checkAuthForInsert(const std::string& ns, const BSONObj& document);
+        Status checkAuthForInsert(const NamespaceString& ns, const BSONObj& document);
 
         // Checks if this connection has the privileges necessary to perform a delete on the given
         // namespace.
-        Status checkAuthForDelete(const std::string& ns, const BSONObj& query);
+        Status checkAuthForDelete(const NamespaceString& ns, const BSONObj& query);
 
-        // Checks if this connection is authorized for the given Privilege.
-        Status checkAuthForPrivilege(const Privilege& privilege);
+        // Returns true if this session is authorized for the given Privilege.
+        //
+        // Contains all the authorization logic including handling things like the localhost
+        // exception.
+        bool isAuthorizedForPrivilege(const Privilege& privilege);
 
-        // Checks if this connection is authorized for all the given Privileges.
-        Status checkAuthForPrivileges(const vector<Privilege>& privileges);
+        // Like isAuthorizedForPrivilege, above, except returns true if the session is authorized
+        // for all of the listed privileges.
+        bool isAuthorizedForPrivileges(const vector<Privilege>& privileges);
+
+        // Utility function for isAuthorizedForPrivilege(Privilege(resource, action)).
+        bool isAuthorizedForActionsOnResource(const ResourcePattern& resource, ActionType action);
+
+        // Utility function for isAuthorizedForPrivilege(Privilege(resource, actions)).
+        bool isAuthorizedForActionsOnResource(const ResourcePattern& resource,
+                                              const ActionSet& actions);
+
+        // Utility function for
+        // isAuthorizedForActionsOnResource(ResourcePattern::forExactNamespace(ns), action).
+        bool isAuthorizedForActionsOnNamespace(const NamespaceString& ns, ActionType action);
+
+        // Utility function for
+        // isAuthorizedForActionsOnResource(ResourcePattern::forExactNamespace(ns), actions).
+        bool isAuthorizedForActionsOnNamespace(const NamespaceString& ns, const ActionSet& actions);
 
     private:
 
         // Checks if this connection is authorized for the given Privilege, ignoring whether or not
-        // we should even be doing authorization checks in general.
-        Status _checkAuthForPrivilegeHelper(const Privilege& privilege);
-
-        // Returns a new privilege that has replaced the actions needed to handle special casing
-        // certain namespaces like system.users and system.profile.  Note that the special handling
-        // of system.indexes takes place in checkAuthForInsert, not here.
-        Privilege _modifyPrivilegeForSpecialCases(const Privilege& privilege);
-
+        // we should even be doing authorization checks in general.  Note: this may acquire a read
+        // lock on the admin database (to update out-of-date user privilege information).
+        bool _isAuthorizedForPrivilege(const Privilege& privilege);
 
         scoped_ptr<AuthzSessionExternalState> _externalState;
 

src/mongo/db/instance.cpp
@@ -147,8 +147,8 @@ namespace mongo {
         QueryMessage q(d);
         BSONObjBuilder b;
 
-        const bool isAuthorized = cc().getAuthorizationSession()->checkAuthorization(
-                AuthorizationManager::SERVER_RESOURCE_NAME, ActionType::inprog);
+        const bool isAuthorized = cc().getAuthorizationSession()->isAuthorizedForActionsOnResource(
+                ResourcePattern::forClusterResource(), ActionType::inprog);
 
         audit::logInProgAuthzCheck(
                 &cc(), q.query, isAuthorized ? ErrorCodes::OK : ErrorCodes::Unauthorized);
@@ -206,8 +206,8 @@ namespace mongo {
         DbMessage d(m);
         QueryMessage q(d);
         BSONObj obj;
-        const bool isAuthorized = cc().getAuthorizationSession()->checkAuthorization(
-                AuthorizationManager::SERVER_RESOURCE_NAME, ActionType::killop);
+        const bool isAuthorized = cc().getAuthorizationSession()->isAuthorizedForActionsOnResource(
+                ResourcePattern::forClusterResource(), ActionType::killop);
         audit::logKillOpAuthzCheck(&cc(),
                                    q.query,
                                    isAuthorized ? ErrorCodes::OK : ErrorCodes::Unauthorized);
@@ -234,8 +234,8 @@ namespace mongo {
     bool _unlockFsync();
     void unlockFsync(const char *ns, Message& m, DbResponse &dbresponse) {
         BSONObj obj;
-        const bool isAuthorized = cc().getAuthorizationSession()->checkAuthorization(
-                AuthorizationManager::SERVER_RESOURCE_NAME, ActionType::unlock);
+        const bool isAuthorized = cc().getAuthorizationSession()->isAuthorizedForActionsOnResource(
+                ResourcePattern::forClusterResource(), ActionType::unlock);
         audit::logFsyncUnlockAuthzCheck(
                 &cc(), isAuthorized ? ErrorCodes::OK : ErrorCodes::Unauthorized);
         if (!isAuthorized) {
@@ -269,13 +269,12 @@ namespace mongo {
         shared_ptr<AssertionException> ex;
 
         try {
-            if (!NamespaceString(d.getns()).isCommand()) {
+            NamespaceString ns(d.getns());
+            if (!ns.isCommand()) {
                 // Auth checking for Commands happens later.
                 Client* client = &cc();
-                Status status = client->getAuthorizationSession()->checkAuthForQuery(d.getns(),
-                                                                                     q.query);
-                audit::logQueryAuthzCheck(
-                        client, NamespaceString(d.getns()), q.query, status.code());
+                Status status = client->getAuthorizationSession()->checkAuthForQuery(ns, q.query);
+                audit::logQueryAuthzCheck(client, ns, q.query, status.code());
                 uassertStatusOK(status);
             }
             dbresponse.exhaustNS = runQuery(m, q, op, *resp);
@@ -588,8 +587,8 @@ namespace mongo {
 
     void receivedUpdate(Message& m, CurOp& op) {
         DbMessage d(m);
-        const char *ns = d.getns();
-        op.debug().ns = ns;
+        NamespaceString ns(d.getns());
+        op.debug().ns = ns.ns();
         int flags = d.pullInt();
         BSONObj query = d.nextJsObj();
 
@@ -607,8 +606,7 @@ namespace mongo {
                                                                            query,
                                                                            toupdate,
                                                                            upsert);
-        audit::logUpdateAuthzCheck(
-                &cc(), NamespaceString(ns), query, toupdate, upsert, multi, status.code());
+        audit::logUpdateAuthzCheck(&cc(), ns, query, toupdate, upsert, multi, status.code());
         uassertStatusOK(status);
 
         op.debug().query = query;
@@ -646,11 +644,11 @@ namespace mongo {
         PageFaultRetryableSection s;
         while ( 1 ) {
             try {
-                Lock::DBWrite lk(ns);
+                Lock::DBWrite lk(ns.ns());
 
                 // void ReplSetImpl::relinquish() uses big write lock so this is thus
                 // synchronized given our lock above.
-                uassert( 17010 ,  "not master", isMasterNs( ns ) );
+                uassert( 17010 ,  "not master", isMasterNs( ns.ns().c_str() ) );
 
                 // if this ever moves to outside of lock, need to adjust check
                 // Client::Context::_finishInit
@@ -682,9 +680,9 @@ namespace mongo {
 
     void receivedDelete(Message& m, CurOp& op) {
         DbMessage d(m);
-        const char *ns = d.getns();
+        NamespaceString ns(d.getns());
 
-        op.debug().ns = ns;
+        op.debug().ns = ns.ns();
         int flags = d.pullInt();
         bool justOne = flags & RemoveOption_JustOne;
         bool broadcast = flags & RemoveOption_Broadcast;
@@ -692,7 +690,7 @@ namespace mongo {
         BSONObj pattern = d.nextJsObj();
 
         Status status = cc().getAuthorizationSession()->checkAuthForDelete(ns, pattern);
-        audit::logDeleteAuthzCheck(&cc(), NamespaceString(ns), pattern, status.code());
+        audit::logDeleteAuthzCheck(&cc(), ns, pattern, status.code());
         uassertStatusOK(status);
 
         op.debug().query = pattern;
@@ -701,10 +699,10 @@ namespace mongo {
         PageFaultRetryableSection s;
         while ( 1 ) {
             try {
-                Lock::DBWrite lk(ns);
+                Lock::DBWrite lk(ns.ns());
                 
                 // writelock is used to synchronize stepdowns w/ writes
-                uassert( 10056 ,  "not master", isMasterNs( ns ) );
+                uassert( 10056 ,  "not master", isMasterNs( ns.ns().c_str() ) );
                 
                 // if this ever moves to outside of lock, need to adjust check Client::Context::_finishInit
                 if ( ! broadcast && handlePossibleShardedMessage( m , 0 ) )
@@ -712,7 +710,7 @@ namespace mongo {
                 
                 Client::Context ctx(ns);
                 
-                long long n = deleteObjects(ns, pattern, justOne, true);
+                long long n = deleteObjects(ns.ns().c_str(), pattern, justOne, true);
                 lastError.getSafe()->recordDelete( n );
                 op.debug().ndeleted = n;
                 break;
@@ -751,8 +749,9 @@ namespace mongo {
                 const NamespaceString nsString( ns );
                 uassert( 16258, str::stream() << "Invalid ns [" << ns << "]", nsString.isValid() );
 
-                Status status = cc().getAuthorizationSession()->checkAuthForGetMore(ns, cursorid);
-                audit::logGetMoreAuthzCheck(&cc(), NamespaceString(ns), cursorid, status.code());
+                Status status = cc().getAuthorizationSession()->checkAuthForGetMore(
+                        nsString, cursorid);
+                audit::logGetMoreAuthzCheck(&cc(), nsString, cursorid, status.code());
                 uassertStatusOK(status);
 
                 if (str::startsWith(ns, "local.oplog.")){
@@ -910,8 +909,9 @@ namespace mongo {
 
             // Check auth for insert (also handles checking if this is an index build and checks
             // for the proper privileges in that case).
-            Status status = cc().getAuthorizationSession()->checkAuthForInsert(ns, obj);
-            audit::logInsertAuthzCheck(&cc(), NamespaceString(ns), obj, status.code());
+            const NamespaceString nsString(ns);
+            Status status = cc().getAuthorizationSession()->checkAuthForInsert(nsString, obj);
+            audit::logInsertAuthzCheck(&cc(), nsString, obj, status.code());
             uassertStatusOK(status);
         }
 

monocle-ai/mongo

Conflicts:
	src/mongo/db/auth/authorization_session.cpp
	src/mongo/db/clientcursor.cpp
	src/mongo/db/commands.h
	src/mongo/db/commands/apply_ops.cpp
	src/mongo/db/commands/cleanup_orphaned_cmd.cpp
	src/mongo/db/commands/cpuprofile.cpp
	src/mongo/db/commands/index_stats.cpp
	src/mongo/db/commands/merge_chunks_cmd.cpp
	src/mongo/db/commands/pipeline_command.cpp
	src/mongo/db/commands/rename_collection_common.cpp
	src/mongo/db/commands/storage_details.cpp
	src/mongo/db/commands/write_commands/write_commands.cpp
	src/mongo/db/compact.cpp
	src/mongo/db/dbcommands.cpp
	src/mongo/db/dbcommands_admin.cpp
	src/mongo/db/exec/stagedebug_cmd.cpp
	src/mongo/db/fts/fts_command.cpp
	src/mongo/db/instance.cpp
	src/mongo/db/pipeline/pipeline.cpp
	src/mongo/db/pipeline/pipeline.h
	src/mongo/db/repl/master_slave.cpp
	src/mongo/db/repl/replset_commands.cpp
	src/mongo/s/commands_public.cpp
	src/mongo/s/merge_chunks_cmd.cpp
	src/mongo/s/shardconnection.cpp
	src/mongo/s/strategy_shard.cpp
Former-commit-id: 172a2785a685c1d8112c00ac60cd0509b439cda2

src/mongo/db/auth/authorization_session.h
@@ -28,15 +28,19 @@
 #include "mongo/db/auth/privilege.h"
 #include "mongo/db/auth/user_name.h"
 #include "mongo/db/auth/user_set.h"
+#include "mongo/db/namespacestring.h"
 
 namespace mongo {
 
     /**
      * Contains all the authorization logic for a single client connection.  It contains a set of
-     * the principals which have been authenticated, as well as a set of privileges that have been
-     * granted by those principals to perform various actions.
-     * An AuthorizationSession object is present within every mongo::Client object, therefore there
-     * is one per thread that corresponds to an incoming client connection.
+     * the users which have been authenticated, as well as a set of privileges that have been
+     * granted to those users to perform various actions.
+     *
+     * An AuthorizationSession object is present within every mongo::ClientBasic object.
+     *
+     * Predicate methods for checking authorization may in the worst case acquire read locks
+     * on the admin database.
      */
     class AuthorizationSession {
         MONGO_DISALLOW_COPYING(AuthorizationSession);
@@ -80,27 +84,17 @@ namespace mongo {
         // Used to grant internal threads full access.
         void grantInternalAuthorization();
 
-        // Checks if this connection has the privileges required to perform the given action
-        // on the given resource.  Contains all the authorization logic including handling things
-        // like the localhost exception.  Returns true if the action may proceed on the resource.
-        // Note: this may acquire a database read lock (for automatic privilege acquisition).
-        bool checkAuthorization(const std::string& resource, ActionType action);
-
-        // Same as above but takes an ActionSet instead of a single ActionType.  Returns true if
-        // all of the actions may proceed on the resource.
-        bool checkAuthorization(const std::string& resource, ActionSet actions);
-
         // Checks if this connection has the privileges necessary to perform the given query on the
         // given namespace.
-        Status checkAuthForQuery(const std::string& ns, const BSONObj& query);
+        Status checkAuthForQuery(const NamespaceString& ns, const BSONObj& query);
 
         // Checks if this connection has the privileges necessary to perform a getMore on the given
         // cursor in the given namespace.
-        Status checkAuthForGetMore(const std::string& ns, long long cursorID);
+        Status checkAuthForGetMore(const NamespaceString& ns, long long cursorID);
 
         // Checks if this connection has the privileges necessary to perform the given update on the
         // given namespace.
-        Status checkAuthForUpdate(const std::string& ns,
+        Status checkAuthForUpdate(const NamespaceString& ns,
                                   const BSONObj& query,
                                   const BSONObj& update,
                                   bool upsert);
@@ -108,29 +102,43 @@ namespace mongo {
         // Checks if this connection has the privileges necessary to insert the given document
         // to the given namespace.  Correctly interprets inserts to system.indexes and performs
         // the proper auth checks for index building.
-        Status checkAuthForInsert(const std::string& ns, const BSONObj& document);
+        Status checkAuthForInsert(const NamespaceString& ns, const BSONObj& document, bool buildingSystemUsersIndex=false);
 
         // Checks if this connection has the privileges necessary to perform a delete on the given
         // namespace.
-        Status checkAuthForDelete(const std::string& ns, const BSONObj& query);
+        Status checkAuthForDelete(const NamespaceString& ns, const BSONObj& query);
 
-        // Checks if this connection is authorized for the given Privilege.
-        Status checkAuthForPrivilege(const Privilege& privilege);
+        // Returns true if this session is authorized for the given Privilege.
+        //
+        // Contains all the authorization logic including handling things like the localhost
+        // exception.
+        bool isAuthorizedForPrivilege(const Privilege& privilege);
 
-        // Checks if this connection is authorized for all the given Privileges.
-        Status checkAuthForPrivileges(const vector<Privilege>& privileges);
+        // Like isAuthorizedForPrivilege, above, except returns true if the session is authorized
+        // for all of the listed privileges.
+        bool isAuthorizedForPrivileges(const vector<Privilege>& privileges);
+
+        // Utility function for isAuthorizedForPrivilege(Privilege(resource, action)).
+        bool isAuthorizedForActionsOnResource(const ResourcePattern& resource, ActionType action);
+
+        // Utility function for isAuthorizedForPrivilege(Privilege(resource, actions)).
+        bool isAuthorizedForActionsOnResource(const ResourcePattern& resource,
+                                              const ActionSet& actions);
+
+        // Utility function for
+        // isAuthorizedForActionsOnResource(ResourcePattern::forExactNamespace(ns), action).
+        bool isAuthorizedForActionsOnNamespace(const NamespaceString& ns, ActionType action);
+
+        // Utility function for
+        // isAuthorizedForActionsOnResource(ResourcePattern::forExactNamespace(ns), actions).
+        bool isAuthorizedForActionsOnNamespace(const NamespaceString& ns, const ActionSet& actions);
 
     private:
 
         // Checks if this connection is authorized for the given Privilege, ignoring whether or not
-        // we should even be doing authorization checks in general.
-        Status _checkAuthForPrivilegeHelper(const Privilege& privilege);
-
-        // Returns a new privilege that has replaced the actions needed to handle special casing
-        // certain namespaces like system.users and system.profile.  Note that the special handling
-        // of system.indexes takes place in checkAuthForInsert, not here.
-        Privilege _modifyPrivilegeForSpecialCases(const Privilege& privilege);
-
+        // we should even be doing authorization checks in general.  Note: this may acquire a read
+        // lock on the admin database (to update out-of-date user privilege information).
+        bool _isAuthorizedForPrivilege(const Privilege& privilege);
 
         scoped_ptr<AuthzSessionExternalState> _externalState;
 

src/mongo/db/instance.cpp
@@ -116,8 +116,8 @@ namespace mongo {
         QueryMessage q(d);
         BSONObjBuilder b;
 
-        const bool isAuthorized = cc().getAuthorizationSession()->checkAuthorization(
-                AuthorizationManager::SERVER_RESOURCE_NAME, ActionType::inprog);
+        const bool isAuthorized = cc().getAuthorizationSession()->isAuthorizedForActionsOnResource(
+                ResourcePattern::forClusterResource(), ActionType::inprog);
 
         audit::logInProgAuthzCheck(
                 &cc(), q.query, isAuthorized ? ErrorCodes::OK : ErrorCodes::Unauthorized);
@@ -167,8 +167,8 @@ namespace mongo {
         DbMessage d(m);
         QueryMessage q(d);
         BSONObj obj;
-        const bool isAuthorized = cc().getAuthorizationSession()->checkAuthorization(
-                AuthorizationManager::SERVER_RESOURCE_NAME, ActionType::killop);
+        const bool isAuthorized = cc().getAuthorizationSession()->isAuthorizedForActionsOnResource(
+                ResourcePattern::forClusterResource(), ActionType::killop);
         audit::logKillOpAuthzCheck(&cc(),
                                    q.query,
                                    isAuthorized ? ErrorCodes::OK : ErrorCodes::Unauthorized);
@@ -205,13 +205,12 @@ namespace mongo {
         shared_ptr<AssertionException> ex;
 
         try {
-            if (!NamespaceString::isCommand(d.getns())) {
+            NamespaceString ns(d.getns());
+            if (!ns.isCommand()) {
                 // Auth checking for Commands happens later.
                 Client* client = &cc();
-                Status status = client->getAuthorizationSession()->checkAuthForQuery(d.getns(),
-                                                                                     q.query);
-                audit::logQueryAuthzCheck(
-                        client, NamespaceString(d.getns()), q.query, status.code());
+                Status status = client->getAuthorizationSession()->checkAuthForQuery(ns, q.query);
+                audit::logQueryAuthzCheck(client, ns, q.query, status.code());
                 uassertStatusOK(status);
             }
             dbresponse.exhaustNS = runQuery(m, q, op, *resp);
@@ -525,8 +524,8 @@ namespace mongo {
 
     void receivedUpdate(Message& m, CurOp& op) {
         DbMessage d(m);
-        const char *ns = d.getns();
-        op.debug().ns = ns;
+        NamespaceString ns(d.getns());
+        op.debug().ns = ns.ns();
         int flags = d.pullInt();
         BSONObj query = d.nextJsObj();
 
@@ -549,8 +548,7 @@ namespace mongo {
                                                                            query,
                                                                            updateobj,
                                                                            upsert);
-        audit::logUpdateAuthzCheck(
-                &cc(), NamespaceString(ns), query, updateobj, upsert, multi, status.code());
+        audit::logUpdateAuthzCheck(&cc(), ns, query, updateobj, upsert, multi, status.code());
         uassertStatusOK(status);
 
         OpSettings settings;
@@ -565,26 +563,26 @@ namespace mongo {
 
         LOCK_REASON(lockReason, "update");
         try {
-            Lock::DBRead lk(ns, lockReason);
-            lockedReceivedUpdate(ns, m, op, updateobj, query, upsert, multi);
+            Lock::DBRead lk(ns.ns(), lockReason);
+            lockedReceivedUpdate(ns.ns().c_str(), m, op, updateobj, query, upsert, multi);
         }
         catch (RetryWithWriteLock &e) {
-            Lock::DBWrite lk(ns, lockReason);
-            lockedReceivedUpdate(ns, m, op, updateobj, query, upsert, multi);
+            Lock::DBWrite lk(ns.ns(), lockReason);
+            lockedReceivedUpdate(ns.ns().c_str(), m, op, updateobj, query, upsert, multi);
         }
     }
 
     void receivedDelete(Message& m, CurOp& op) {
         DbMessage d(m);
-        const char *ns = d.getns();
+        NamespaceString ns(d.getns());
 
-        op.debug().ns = ns;
+        op.debug().ns = ns.ns();
         int flags = d.pullInt();
         verify(d.moreJSObjs());
         BSONObj pattern = d.nextJsObj();
 
         Status status = cc().getAuthorizationSession()->checkAuthForDelete(ns, pattern);
-        audit::logDeleteAuthzCheck(&cc(), NamespaceString(ns), pattern, status.code());
+        audit::logDeleteAuthzCheck(&cc(), ns, pattern, status.code());
         uassertStatusOK(status);
 
         op.debug().query = pattern;
@@ -604,16 +602,16 @@ namespace mongo {
         }
 
         LOCK_REASON(lockReason, "delete");
-        Lock::DBRead lk(ns, lockReason);
+        Lock::DBRead lk(ns.ns(), lockReason);
 
         // writelock is used to synchronize stepdowns w/ writes
-        uassert(10056, "not master", isMasterNs(ns));
+        uassert(10056, "not master", isMasterNs(ns.ns().c_str()));
 
-        Client::Context ctx(ns);
+        Client::Context ctx(ns.ns());
         long long n;
-        scoped_ptr<Client::AlternateTransactionStack> altStack(opNeedsAltTxn(ns) ? new Client::AlternateTransactionStack : NULL);
+        scoped_ptr<Client::AlternateTransactionStack> altStack(opNeedsAltTxn(ns.ns()) ? new Client::AlternateTransactionStack : NULL);
         Client::Transaction transaction(DB_SERIALIZABLE);
-        n = deleteObjects(ns, pattern, justOne, true);
+        n = deleteObjects(ns.ns().c_str(), pattern, justOne, true);
         transaction.commit();
 
         lastError.getSafe()->recordDelete( n );
@@ -645,10 +643,12 @@ namespace mongo {
         while( 1 ) {
             bool isCursorAuthorized = false;
             try {
+                const NamespaceString nsString( ns );
                 uassert( 16258, str::stream() << "Invalid ns [" << ns << "]", NamespaceString::isValid(ns) );
 
-                Status status = cc().getAuthorizationSession()->checkAuthForGetMore(ns, cursorid);
-                audit::logGetMoreAuthzCheck(&cc(), NamespaceString(ns), cursorid, status.code());
+                Status status = cc().getAuthorizationSession()->checkAuthForGetMore(
+                        nsString, cursorid);
+                audit::logGetMoreAuthzCheck(&cc(), nsString, cursorid, status.code());
                 uassertStatusOK(status);
 
                 // I (Zardosht), am not crazy about this, but I cannot think of
@@ -918,8 +918,11 @@ namespace mongo {
 
             // Check auth for insert (also handles checking if this is an index build and checks
             // for the proper privileges in that case).
-            Status status = cc().getAuthorizationSession()->checkAuthForInsert(ns, obj);
-            audit::logInsertAuthzCheck(&cc(), NamespaceString(ns), obj, status.code());
+            const NamespaceString nsString(ns);
+            Status status = cc().getAuthorizationSession()->checkAuthForInsert(nsString, obj,
+                                                                               !(cc().creatingSystemUsersOn(nsString) ||
+                                                                                 cc().upgradingDiskFormatVersion()));
+            audit::logInsertAuthzCheck(&cc(), nsString, obj, status.code());
             uassertStatusOK(status);
         }
 